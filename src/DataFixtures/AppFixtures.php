289e3698b83e23d4b523318748cbfc890e3bb8cb
DNG13 <duka.natalja@gmail.com>
2021-11-09 15:46:10 +0200

    Handling ManyToMany in Foundry

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,17 +2,21 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Tag;
 use App\Factory\AnswerFactory;
 use App\Factory\QuestionFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Factory\TagFactory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $questions = QuestionFactory::createMany(20);
+        TagFactory::createMany(100);
+
+        $questions = QuestionFactory::createMany(20, [
+            'tags' => TagFactory::randomRange(0,5),
+        ]);
 
         QuestionFactory::new()
             ->unpublished()
@@ -31,21 +35,6 @@ class AppFixtures extends Fixture
             ];
         })->needsApproval()->many(20)->create();
 
-        $question = QuestionFactory::createOne()->object();
-
-        $tag1 = new Tag();
-        $tag1->setName('dino');
-        $tag2 = new Tag();
-        $tag2->setName('truck');
-
-//        $question->addTag($tag1);
-//        $question->addTag($tag2);
-
-        $tag1->addQuestion($question);
-        $tag2->addQuestion($question);
-
-        $manager->persist($tag1);
-        $manager->persist( $tag2);
         $manager->flush();
     }
 }
